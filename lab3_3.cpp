605621dbc0fef4bde86b1d1839abccad3dc4d56e
lab302-15 <lab302-15@labeng.local>
2019-01-11 17:17:15 +0700

    version1

--- lab3_3.cpp
+++ lab3_3.cpp
@@ -1,1 +1,14 @@
 #include<iostream>
+using namespace std;
+
+int main()
+{
+    double sum=0;
+    double A=2;
+    while(A <= 69){
+        sum += 1/A;
+        A++;
+    }
+    cout<<sum;
+    return 0;
+}
